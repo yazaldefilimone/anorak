d806117e92ad331deab69b922ebf12d977b8daef
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-29 21:01:53 +0200

    feat: create connection of baileys

--- src/main/baileys/index.ts
+++ src/main/baileys/index.ts
@@ -1,11 +1,1 @@
-import { connect } from '@/main/settings';
-
-export default async function () {
-  const client = await connect();
-  client.ev.on('messages.upsert', async (messages) => {
-    console.log(JSON.stringify(messages, undefined, 2));
-
-    console.log('replying to', messages.messages[0].key.remoteJid);
-    await client.sendMessage(messages.messages[0].key.remoteJid!, { text: 'Hello there!' });
-  });
-}
+export * from './connect';

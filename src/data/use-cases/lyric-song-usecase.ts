395c4cc00fc90d5be38b917c87a823b5fe4a9dd7
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-30 10:23:45 +0200

    feat: remove errror param

--- src/data/use-cases/lyric-song-usecase.ts
+++ src/data/use-cases/lyric-song-usecase.ts
@@ -1,4 +1,4 @@
-import { NotfoundError } from '@/domain/errors';
+import { NotfoundError, UnexpectedError } from '@/domain/errors';
 import { ILyricSongUseCase } from '@/domain/use-cases';
 import * as Genius from 'genius-lyrics';
 
@@ -9,12 +9,19 @@ export class LyricSongUseCase implements ILyricSongUseCase {
   }
 
   async perform({ name }: ILyricSongUseCase.input): ILyricSongUseCase.model {
-    const result = await this.genius.songs.search(name);
-    if (!result || !result[0]) {
-      return new NotfoundError('lyric');
-    }
+    try {
+      const result = await this.genius.songs.search(name);
+      if (!result || !result[0]) {
+        return new NotfoundError();
+      }
 
-    const lyric = await result[0].lyrics();
-    return { lyric };
+      const lyric = await result[0].lyrics();
+      return { lyric };
+    } catch (err: any) {
+      if (err.message == 'No result was found') {
+        return new NotfoundError();
+      }
+      return new UnexpectedError();
+    }
   }
 }

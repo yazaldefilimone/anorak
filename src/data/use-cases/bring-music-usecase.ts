b54e0bb5035f15455609523364c1408b784a7dcf
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-30 15:52:42 +0200

    feat: create music dowloand

--- src/data/use-cases/bring-music-usecase.ts
+++ src/data/use-cases/bring-music-usecase.ts
@@ -3,8 +3,9 @@ import YouTubeMp3down, { IVideoTask } from 'youtube-mp3-downloader';
 import { IAlgorithmTransformBytes, IYouTubeSearchHelper } from '@/data/protocols/helpers';
 import { WASocket } from '@adiwajshing/baileys';
 import { Letter } from '@/shared/letter';
-import { botResponse } from '@/main/presentation/helpers';
+import { errorResponse, response } from '@/main/presentation/helpers';
 import { promisify } from 'util';
+import { UnexpectedError } from '@/domain/errors';
 
 type progress = {
   id: string;
@@ -18,12 +19,13 @@ type progress = {
     delta: number;
     speed: number;
   };
+  index: boolean;
 };
 export class BringMusicUseCase implements IBringMusicUseCase {
   private readonly youtubeMp3down: YouTubeMp3down;
   private readonly youTubeSearchHelper: IYouTubeSearchHelper;
   private readonly videoInLoader: IVideoTask;
-  private VideoProgress: progress;
+  private VideoProgress: progress[];
   private readonly algorithmTransformBytes: IAlgorithmTransformBytes;
   constructor(
     youtubeMp3down: YouTubeMp3down,
@@ -33,6 +35,7 @@ export class BringMusicUseCase implements IBringMusicUseCase {
     this.youtubeMp3down = youtubeMp3down;
     this.youTubeSearchHelper = youTubeSearchHelper;
     this.algorithmTransformBytes = algorithmTransformBytes;
+    this.VideoProgress = [];
   }
 
   async perform({ name }: IBringMusicUseCase.input, socket: WASocket, currentUser: string): Promise<any> {
@@ -44,34 +47,30 @@ export class BringMusicUseCase implements IBringMusicUseCase {
 
       this.youtubeMp3down.download(resultSearch.id);
 
-      let youtubeProgressAsync = promisify(this.youtubeMp3down.on);
-      this.VideoProgress = (await youtubeProgressAsync('progress')) as any;
+      this.youtubeMp3down.on('progress', async ({ videoId, progress }) => {
+        const isExists = this.VideoProgress.find((content) => content.id == videoId);
+        if (isExists) {
+          return;
+        } else {
+          this.VideoProgress.push({ id: videoId, video: progress, index: true });
 
-      // this.youtubeMp3down.on('progress', ({ videoId, progress }) => {
-      //   this.VideoProgress.push({ id: videoId, video: progress });
-      //   console.log({ VideoProgress: this.VideoProgress });
-      // });
-
-      const sendFeedBack = async () => {
-        const image = resultSearch.thumbnails ? resultSearch.thumbnails.url : '';
-        const preMessage = `
-          Estou processando o teu pedido. Baixando a  Musica com o nome: ${Letter.bold(resultSearch.title)}
-          Velocidade do download: ${this.algorithmTransformBytes.execute(this.VideoProgress.video.speed)}
-          \nPeco para Aguardar ${this.VideoProgress.video.eta} segundos que ja mando a tua musica
-          Percentagem:${parseFloat(String(this.VideoProgress.video.percentage)).toFixed(2)} %
-          `;
-        await socket.sendMessage(currentUser, { image: { url: image }, caption: botResponse(preMessage, true) });
-      };
+          const image = resultSearch.thumbnails ? resultSearch.thumbnails.url : '';
+          const preMessage = `\n\nBaixando a  Musica com o nome: ${Letter.bold(resultSearch.title)}\n\n${
+            progress.eta
+          } segundos pra o envio da musica\n${Letter.bold(`${parseFloat(String(progress.percentage)).toFixed(2)}%`)}`;
+          await socket.sendMessage(currentUser, { image: { url: image }, caption: response(preMessage, true) });
+        }
+      });
 
       this.youtubeMp3down.on('error', async (error) => {
-        await socket.sendMessage(currentUser, { text: botResponse('Ouve um Erro ') });
+        await socket.sendMessage(currentUser, errorResponse(new UnexpectedError().message));
       });
 
       this.youtubeMp3down.on('finished', async (err, data) => {
         await socket.sendMessage(currentUser, { audio: { url: data.file }, mimetype: 'audio/mp4' });
       });
-
-      setTimeout(async () => await sendFeedBack(), 3000);
-    } catch (err) {}
+    } catch (err: any) {
+      await socket.sendMessage(currentUser, errorResponse(new UnexpectedError().message));
+    }
   }
 }
--- src/main/presentation/helpers/index.ts
+++ src/main/presentation/helpers/index.ts
@@ -1,6 +1,18 @@
-export const botResponse = (message: string, type = true) => {
-  return `
-  ┏ 🤖  *ANORAK* 🤖
-  ┷┯ ☾ *${type ? 'Bem Sucesso 🥳🤩✨' : 'Ouve Um Erro ❌❗'}* ☽\n${message}
+import { resolve } from 'path';
+
+const error = resolve(__dirname, '..', '..', '..', '..', 'cache', 'cache', 'status', 'error.jpg');
+export const errorResponse = (message: string) => {
+  return {
+    image: { url: error },
+    caption: `
+    ┏ 🤖  *ANORAK* 🤖
+    ┷┯ ☾ *Status: Error ❌❗* ☽\n${message}
+    `,
+  };
+};
+
+export const response = (message: string, type = true) => {
+  return `┏ 🤖  *ANORAK* 🤖
+  ┷┯ ☾ ${type ? '*Status: Sucesso 🥳🤩✨*' : '*Status: Sucesso 🥳🤩✨*'} ☽\n${message}
   `;
 };

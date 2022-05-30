f2741a877cd6a1d304c8d180e43fca2d3cb0a35a
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-30 10:24:46 +0200

    feat: create function of find music

--- src/data/use-cases/bring-music-usecase.ts
+++ src/data/use-cases/bring-music-usecase.ts
@@ -1,25 +1,36 @@
 import { IBringMusicUseCase } from '@/domain/use-cases';
 import { HttpStatusCode, IHttpClient } from '@/data/protocols/http';
 import { NotfoundError, UnexpectedError } from '@/domain/errors';
+import YouTubeMp3down, { IVideoTask } from 'youtube-mp3-downloader';
+import YouTubeSearch from 'youtube-search';
+import { IYouTubeSearchHelper } from '@/data/protocols/youtube-search-helper';
 
 export class BringMusicUseCase implements IBringMusicUseCase {
-  private readonly httpClient: IHttpClient;
-  private readonly url: string;
+  private readonly youtubeMp3down: YouTubeMp3down;
+  private readonly youTubeSearchHelper: IYouTubeSearchHelper;
+  private readonly videoInLoader: IVideoTask;
 
-  constructor(httpClient: IHttpClient, url: string) {
-    this.httpClient = httpClient;
-    this.url = url;
+  constructor(youtubeMp3down: YouTubeMp3down, youTubeSearchHelper: IYouTubeSearchHelper) {
+    this.youtubeMp3down = youtubeMp3down;
+    this.youTubeSearchHelper = youTubeSearchHelper;
   }
 
-  async perform(data: IBringMusicUseCase.input): IBringMusicUseCase.model {
-    const result = await this.httpClient.request({ url: this.url, method: 'post', body: data });
-    switch (result.statusCode) {
-      case HttpStatusCode.ok:
-        return result.body;
-      case HttpStatusCode.notFound:
-        return new NotfoundError('musica');
-      default:
-        return new UnexpectedError();
-    }
+  async perform({ name }: IBringMusicUseCase.input): IBringMusicUseCase.model {
+    try {
+      const resultSearch = await this.youTubeSearchHelper.find({ name });
+      if (resultSearch instanceof Error) {
+        return resultSearch;
+      }
+
+      this.youtubeMp3down.download(resultSearch.id);
+
+      this.youtubeMp3down.on('progress', (data) => {
+        this.videoInLoader;
+      });
+
+      this.youtubeMp3down.on('error', function (error, data) {});
+
+      this.youtubeMp3down.on('finished', function (error, data) {});
+    } catch (err) {}
   }
 }

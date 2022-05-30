93b70e888fdd0051f794d0e4540771ef484985bc
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-05-30 11:24:29 +0200

    feat: create configs of youtube

--- src/main/settings/index.ts
+++ src/main/settings/index.ts
@@ -1,9 +1,15 @@
+import { env } from '@/shared/env';
 import { resolve } from 'path';
-export const youtubeConfig = {
+import { YouTubeSearchOptions } from 'youtube-search';
+export const youtubeDownConfig = {
   ffmpegPath: '/usr/bin/ffmpeg',
   outputPath: resolve(__dirname, '..', '..', '..', 'cache', 'mp3'), // Output file location (default: the home directory)
   youtubeVideoQuality: 'highestaudio', // Desired video quality (default: highestaudio)
   queueParallelism: 2, // Download parallelism (default: 1)
   progressTimeout: 2000, // Interval in ms for the progress reports (default: 1000)
   allowWebm: false, // Enable download from WebM sources (default: false)
+};
+export const youtubeSearchConfig: YouTubeSearchOptions = {
+  maxResults: 3,
+  key: env.google.key,
 };

09fe641809df098892f717fe934feb7b79956ba5
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-19 17:36:06 +0200

    fix: change method od get my name

--- src/main/presentation/commands/contracts/index.ts
+++ src/main/presentation/commands/contracts/index.ts
@@ -3,26 +3,30 @@ import { Letter } from '@/shared/letter';
 export const contracts = {
   about: () => {
     return `
-    ┏ 🤖  * Anorak - Bot * 🤖
+    ┏ 🤖  *Anorak - Bot* 🤖
   ┯┷ ☾ Study Commands ☽
   ╽
-  ┠❥ *!biblia [mateus 12:2] - (Retorna a palavra que esta na bíblia)*
+  ┠❥ _!biblia [mateus 12:2]_
   ╿
   ┯┷ ☾ Downloader Commandos ☽
   ╽
-  ┠❥ *!mp3 [nome do artista/musica] - (faz o download da musica)*
-  ┠❥ *!letra [nome do artista/musica] - (faz uma busca da letra da musica)*
-  ┠❥ *!facebook [link do video facebook]*
+  ┠❥ _!mp3 [nome do artista/musica]_
+  ┠❥ _!letra [nome do artista/musica]_
+  ┠❥ _!facebook [link do video facebook]_
   ╿
   ┷┯ ☾ Outros Commandos ☽
   ╽
-  ┠❥ *!sobre mim*
-  ┠❥ *!sobre*
+  ┠❥ _!sobre mim_
+  ┠❥ _!sobre_
+  ┠❥ _!comandos_
+  ┠❥ _!menu_
   ╰╼❥ Envie o comando *!sobre* para saber mais sobre!!.
     `;
   },
 
   owner: () => {
-    return `${Letter.monoSpace(`Hi There!!\nEu sou  Yazalde Filimone  tenho 17 anos e sou  Apenas um garoto introvertido que gosta de animes e cosplay\n\nSoftware Engineer na A4PM - Analytics for Public Management. \n\nSou entusiasta da computação académica e sempre em constante aprendizado\n\nMeu Site: https://yazaldefilimone.vercel.app/\n\nMeu Repositório:https://github.com/yazaldefilimonepinto`)}`;
+    return `${Letter.monoSpace('Hi There!!\nMe chamo')}  *Yazalde Filimone*  ${Letter.monoSpace('tenho')} *18 anos* ${Letter.monoSpace(
+      'e sou  Apenas um garoto introvertido que gosta de animes e cosplay\n\nSoftware Engineer na A4PM - Analytics for Public Management. \n\nSou entusiasta da computação académica e sempre em constante aprendizado\n\n',
+    )}*Meu Site:* https://yazaldefilimone.vercel.app/\n\n*Meu Repositório:* https://github.com/yazaldefilimonepinto`;
   },
 };

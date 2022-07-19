08922e985011ee6d8cbaf5b815f492075f06f41c
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-19 18:58:32 +0200

    feat: remove express

--- src/main/start.ts
+++ src/main/start.ts
@@ -1,13 +1,6 @@
 import './settings/alias';
 import express from 'express';
 import { connect } from './baileys';
-const app = express();
 connect({
   printQRInTerminal: true,
-}).then(() => {
-  app.listen(process.env.PORT as string, () => console.log(`log in port: ${process.env.PORT}`));
-
-  app.get('/', (req, res) => {
-    res.send({ ok: 'hello anorak bot!!!' });
-  });
-});
+}).then(() => {});

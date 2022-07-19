e31c5d832101340d3cb6381e0f346315d17ec457
Yazalde Filimone <yazaldefilimon@gmail.com>
2022-07-19 18:30:02 +0200

    feat: add express

--- src/main/start.ts
+++ src/main/start.ts
@@ -1,9 +1,13 @@
 import './settings/alias';
-
+import express from 'express';
 import { connect } from './baileys';
-
+const app = express();
 connect({
   printQRInTerminal: true,
-}).then(() => console.log('O servidor esta rodando'));
+}).then(() => {
+  app.listen(process.env.PORT as string, () => console.log(`log in port: ${process.env.PORT}`));
 
-
+  app.get('/', (req, res) => {
+    res.send({ ok: 'hello anorak bot!!!' });
+  });
+});
